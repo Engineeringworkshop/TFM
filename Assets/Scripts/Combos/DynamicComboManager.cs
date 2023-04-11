7ef062fbd7c94b53823dfe410d3c6e5b6fa50998
PABLO <pablo.molina.parellada@gmail.com>
2023-04-11 21:24:19 +0200

    Combo control spawn targets

--- Assets/Scripts/Combos/DynamicComboManager.cs
+++ Assets/Scripts/Combos/DynamicComboManager.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class DynamicComboManager : MonoBehaviour
 {
+    [Header("Node spawn configuration")]
     [SerializeField] private Transform targetReferenceRadius;
     [SerializeField] private List<Target> currentTargetList;
     [SerializeField] private GameObject cursor;
-    [SerializeField] private GameObject target;
+    [SerializeField] private GameObject targetObj;
 
+    [Header("Debug")]
+    [SerializeField] private List<GameObject> targetSpawnedList;
     [SerializeField] private float referenceRadius;
 
     private void Start()
     {
-        LoadTargets();
+        referenceRadius = Vector3.Distance(cursor.transform.position, targetReferenceRadius.position);
 
-        referenceRadius = Vector3.Distance(transform.position, targetReferenceRadius.position);
+        LoadTargets();
     }
 
     // Method to load targets on the screen
@@ -23,13 +26,57 @@ public class DynamicComboManager : MonoBehaviour
     {
         // Set the initial target position
         float randomAngle = Random.Range(0, 2 * Mathf.PI);
-        Vector3 currentPosition = new Vector3(transform.position.x*Mathf.Cos(randomAngle), transform.position.y * Mathf.Sin(randomAngle), transform.position.z);
+
+        bool isFirstTarget = false;
+
+        Vector3 currentPosition;
+
+        Vector3 previousPosition = transform.position;
 
         foreach (var target in currentTargetList)
         {
-            // Spawn targets
-            var currentTarget = Instantiate(target, currentPosition, transform.rotation, transform.parent);
-            currentTargetList.Add(currentTarget);
+            if (!isFirstTarget)
+            {
+                // Spawn target
+                GameObject currentTarget = Instantiate(targetObj, transform.position, transform.rotation, cursor.transform.parent);
+
+                // Move target to the final position
+                currentPosition = new Vector3(transform.position.x + referenceRadius * Mathf.Cos(randomAngle), transform.position.y + referenceRadius * Mathf.Sin(randomAngle), transform.position.z);
+
+                Debug.Log("Parent 1 pos: " + transform.position + " Calculated pos: " + currentPosition);
+
+                currentTarget.transform.position = currentPosition;
+
+                previousPosition = currentPosition;
+
+                Debug.Log("Target 1 pos: " + currentPosition);
+
+                isFirstTarget = true;
+
+                targetSpawnedList.Add(currentTarget);
+            }
+            else
+            {
+                // Spawn target
+                GameObject currentTarget = Instantiate(targetObj, previousPosition, transform.rotation, cursor.transform.parent);
+
+                // Rotate target
+                currentTarget.transform.Rotate(cursor.transform.forward, 360/currentTargetList.Count);
+
+                currentPosition = currentTarget.transform.position;
+
+                targetSpawnedList.Add(currentTarget);
+
+                Debug.Log("Target 2 pos: " + currentPosition);
+            }
+        }
+    }
+
+    private void DestroyTargets()
+    {
+        foreach (var target in targetSpawnedList)
+        {
+            Destroy(target);
         }
     }
 
@@ -38,6 +85,8 @@ public class DynamicComboManager : MonoBehaviour
         if (collision.collider.tag == "Target")
         {
             Debug.Log("Target reached");
+
+            DestroyTargets();
         }
     }
 
